ccd3a93c3ba53348aeea20dc3add2309fa36e37c
Roshini133 <btechcse230228@smvec.ac.in>
2026-06-16 14:10:32 +0530

    Add files via upload

---
+++ static/js/dashboard.js
@@ -0,0 +1,4 @@
+
+new Chart(document.getElementById('c1'),{type:'line',data:{labels:['Mon','Tue','Wed','Thu','Fri'],datasets:[{data:[8,7,9,6,5]}]}});
+new Chart(document.getElementById('c2'),{type:'doughnut',data:{labels:['Instagram','YouTube','WhatsApp'],datasets:[{data:[45,35,20]}]}});
+new Chart(document.getElementById('c3'),{type:'bar',data:{labels:['Mon','Tue','Wed','Thu','Fri'],datasets:[{label:'Sleep',data:[6,5,7,8,7]},{label:'Screen',data:[8,7,9,6,5]}]}});
